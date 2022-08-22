491771fe460d34c7dc32096676a0fd767d2c8c6f
onedrako <onedrako@gmail.com>
2022-08-22 13:14:12 -0500

    Multiple text area question working

--- src/Components/Inputs.jsx
+++ src/Components/Inputs.jsx
@@ -3,23 +3,19 @@ import React from 'react'
 const Inputs = ({ data, formikConfig }) => {
   const { id, question, questionText, questionType, textInputs, img } = data
 
-  const options = ['a', 'b', 'c', 'd', 'e']
-
-  // console.log(data)
-
   return (
     <div>
       <h3>{`${id}. ${questionText}`}</h3>
-      {questionType === 'multiple' &&
-        Object.values(textInputs).map((value, index) =>
-        <>
-        <div key={`${value}-${index}`}>
-          {value}
 
-          {/* <textarea {...formikConfig.getFieldProps('q1.a')} ></textarea> */}
-          <textarea {...formikConfig.getFieldProps(`${value.question}`)} ></textarea>
+      {/* If component has multiple text area */}
+      {questionType === 'multiple' &&
+        Object.keys(textInputs).map((key, index) =>
+
+        <div key={`${question}-${textInputs.key}-${index}`}>
+          {textInputs[key]}
+          <textarea {...formikConfig.getFieldProps(`${question}.${key}`)} ></textarea>
         </div>
-        </>
+
         )
         }
     </div>
